e91e0128fd0fc5707c5763c8d8896b602cf09548
Boris Berenberg <boris@atlasauthority.com>
2017-07-31 13:26:43 -0400

    1.3.1 release

--- src/main/java/com/atlassian/plugins/confluence/markdown/MarkdownMacro.java
+++ src/main/java/com/atlassian/plugins/confluence/markdown/MarkdownMacro.java
@@ -10,6 +10,7 @@ import com.atlassian.confluence.xhtml.api.MacroDefinitionHandler;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +18,21 @@ import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.BaseMacro;
 import com.atlassian.renderer.v2.macro.MacroException;
-import org.pegdown.Parser;
-import org.pegdown.PegDownProcessor;
+
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughSubscriptExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+//import com.vladsch.flexmark.ext.superscript.SuperscriptExtension;
+import com.vladsch.flexmark.ext.ins.InsExtension;
+import com.vladsch.flexmark.ext.definition.DefinitionExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension;
+import com.vladsch.flexmark.ext.wikilink.WikiLinkExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.options.MutableDataSet;
+
+
 
 public class MarkdownMacro extends BaseMacro implements Macro
 {
@@ -44,9 +58,30 @@ public class MarkdownMacro extends BaseMacro implements Macro
     @Override
     public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
     {
-        PegDownProcessor translator = new PegDownProcessor(Parser.ALL);
-        String output = translator.markdownToHtml(bodyContent);
-        return output;
+        MutableDataSet options = new MutableDataSet();
+
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+            TablesExtension.create(), 
+            StrikethroughSubscriptExtension.create(),
+//            SuperscriptExtension.create(),
+            InsExtension.create(),
+            TaskListExtension.create(),
+            FootnoteExtension.create(),
+            WikiLinkExtension.create(),
+            DefinitionExtension.create()
+        ));
+
+        
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse(bodyContent);
+        String html = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        return html;
+
+        //PegDownProcessor translator = new PegDownProcessor(Parser.ALL);
+        //String output = translator.markdownToHtml(bodyContent);
+        //return output;
     }
 
     @Override
