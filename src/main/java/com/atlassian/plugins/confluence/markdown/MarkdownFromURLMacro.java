27dcf6d8bf7b411c0a8473105942182ff34edb9c
unknown <joshua@interoctiv.io>
2018-08-10 16:27:26 -0600

    Added error handling for a variety of errors.

--- src/main/java/com/atlassian/plugins/confluence/markdown/MarkdownFromURLMacro.java
+++ src/main/java/com/atlassian/plugins/confluence/markdown/MarkdownFromURLMacro.java
@@ -24,6 +24,7 @@ import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.webresource.api.assembler.PageBuilderService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import com.vladsch.flexmark.ast.Node;
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughSubscriptExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
@@ -39,6 +40,7 @@ import com.vladsch.flexmark.ext.youtube.embedded.YouTubeLinkExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
+
 
 import java.net.*;
 import java.io.*;
@@ -78,37 +80,44 @@ public class MarkdownFromURLMacro extends BaseMacro implements Macro
     public String execute(Map<String, String> parameters, String bodyContent, ConversionContext conversionContext) throws MacroExecutionException
     {
 
-
-        pageBuilderService.assembler().resources().requireWebResource("com.atlassian.plugins.confluence.markdown.confluence-markdown-macro:highlightjs");
-
-        MutableDataSet options = new MutableDataSet();
-
-        options.set(Parser.EXTENSIONS, Arrays.asList(
-            TablesExtension.create(), 
-            StrikethroughSubscriptExtension.create(),
-            InsExtension.create(),
-            TaskListExtension.create(),
-            FootnoteExtension.create(),
-            WikiLinkExtension.create(),
-            DefinitionExtension.create(),
-            AnchorLinkExtension.create(),
-            AutolinkExtension.create(),
-            SuperscriptExtension.create(),
-            YouTubeLinkExtension.create()
-
-        ));
-
-
-        String highlightjs = "<script>\n" +
-                "AJS.$('[data-macro-name=\"markdown\"] code').each(function(i, block) {\n" +
-                "    hljs.highlightBlock(block);\n" +
-                "  });\n" +
-                "</script>";
-
 		if (bodyContent != null) {
+			pageBuilderService.assembler().resources().requireWebResource("com.atlassian.plugins.confluence.markdown.confluence-markdown-macro:highlightjs");
+
+			MutableDataSet options = new MutableDataSet();
+
+			options.set(Parser.EXTENSIONS, Arrays.asList(
+				TablesExtension.create(), 
+				StrikethroughSubscriptExtension.create(),
+				InsExtension.create(),
+				TaskListExtension.create(),
+				FootnoteExtension.create(),
+				WikiLinkExtension.create(),
+				DefinitionExtension.create(),
+				AnchorLinkExtension.create(),
+				AutolinkExtension.create(),
+				SuperscriptExtension.create(),
+				YouTubeLinkExtension.create()
+
+			));
+
+
+			String highlightjs = "<script>\n" +
+					"AJS.$('[data-macro-name=\"markdown\"] code').each(function(i, block) {\n" +
+					"    hljs.highlightBlock(block);\n" +
+					"  });\n" +
+					"</script>";
+
+			class privateRepositoryException extends Exception {
+				public privateRepositoryException(String message) {
+					super(message);
+				}
+			}
+			
 			Parser parser = Parser.builder(options).build();
 			HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 			
+			String exceptionsToReturn = "";
+			String html = "";
 			String toParse = "";
 			try {
 				URL importFrom = new URL(bodyContent);
@@ -116,21 +125,39 @@ public class MarkdownFromURLMacro extends BaseMacro implements Macro
 					new InputStreamReader(importFrom.openStream())
 				);
 				String inputLine;
-				toParse = "";
 				while ((inputLine = in.readLine()) != null) {
 					toParse = toParse + "\n" + inputLine;
 				}
 				in.close();
+				toParse = toParse.trim();
+				if (toParse.startsWith("<html>\n<head>\n  <title>OpenID transaction in progress</title>")) {
+					throw new privateRepositoryException("Cannot import from private repository.");
+				}else {
+					Node document = parser.parse(toParse);
+					html = renderer.render(document) + highlightjs;
+				}
 			}
-			catch (IOException e) {}
-			
-			Node document = parser.parse(toParse);
-			String html = renderer.render(document) + highlightjs;
-			return html;
+			catch (MalformedURLException u) {
+				exceptionsToReturn = exceptionsToReturn + "<strong>Error with Markdown From URL macro: Invalid URL.</strong><br>Please enter a valid URL. If you are not trying to import markdown from a URL, use the Markdown macro instead of the Markdown from URL macro.<br>For support <a href='https://community.atlassian.com/t5/tag/addon-com.atlassian.plugins.confluence.markdown.confluence-markdown-macro/tg-p'>visit our Q&A in the Atlassian Community</a>. You can ask a new question by clicking the \"Create\" button on the top right of the Q&A.<br>";
+			}
+			catch (privateRepositoryException p) {
+				exceptionsToReturn = exceptionsToReturn + "<strong>Error with Markdown From URL macro: Importing from private Bitbucket repositories is not supported.</strong><br>Please make your repository public before importing. Alternatively, you can copy and paste your markdown into the Markdown macro.<br>If you are allowed access, you can find the markdown file <a href='" + bodyContent + "'>here</a>.<br>For support <a href='https://community.atlassian.com/t5/tag/addon-com.atlassian.plugins.confluence.markdown.confluence-markdown-macro/tg-p'>visit our Q&A in the Atlassian Community</a>. You can ask a new question by clicking the \"Create\" button on the top right of the Q&A.<br>";
+			}
+			catch (FileNotFoundException f) {
+				exceptionsToReturn = exceptionsToReturn + "<strong>Error with Markdown From URL macro: URL does not exist.</strong><br>" + bodyContent + "<br>Please double check your URL. Perhaps you made a typo or perhaps the page has been moved.<br>This can also be caused by changing the Github repository containing the file from public to private. If this is the case go back to the raw file and re-copy the link.<br>For support <a href='https://community.atlassian.com/t5/tag/addon-com.atlassian.plugins.confluence.markdown.confluence-markdown-macro/tg-p'>visit our Q&A in the Atlassian Community</a>. You can ask a new question by clicking the \"Create\" button on the top right of the Q&A.<br>";
+			}
+			catch (IOException e) {
+				exceptionsToReturn = exceptionsToReturn + "<strong>Error with Markdown From URL macro: Unexpected error.</strong><br>" + e.toString() + "<br>For support <a href='https://community.atlassian.com/t5/tag/addon-com.atlassian.plugins.confluence.markdown.confluence-markdown-macro/tg-p'>visit our Q&A in the Atlassian Community</a>. You can ask a new question by clicking the \"Create\" button on the top right of the Q&A.<br>"; 
+			}
+			finally {
+				if (exceptionsToReturn != "") {
+					html = "<p style='background: #ffe0e0; border-radius: 5px; padding: 10px;'>" + exceptionsToReturn + "</p>";
+				}
+				return html;
+			}
  		}else {
 			return "";
 		}
-
     }
 
     @Override
