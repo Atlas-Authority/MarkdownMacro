6244726b5d7672212e76f42c86010109f8fb86ab
Nhan Nguyen <nhan.nguyen@atlasauthority.com>
2022-01-17 15:40:41 +0700

    Resolve deprecated code

--- src/main/java/com/atlassian/plugins/confluence/markdown/ccma/MyPluginComponentImpl.java
+++ src/main/java/com/atlassian/plugins/confluence/markdown/ccma/MyPluginComponentImpl.java
@@ -1,10 +1,16 @@
 package com.atlassian.plugins.confluence.markdown.ccma;
 
+import com.atlassian.confluence.api.model.content.Content;
+import com.atlassian.confluence.api.model.content.ContentBody;
+import com.atlassian.confluence.api.model.content.ContentRepresentation;
+import com.atlassian.confluence.api.model.content.ContentType;
+import com.atlassian.confluence.api.model.content.id.ContentId;
+import com.atlassian.confluence.api.service.content.ContentService;
 import com.atlassian.confluence.content.render.xhtml.DefaultConversionContext;
-import com.atlassian.confluence.pages.Page;
-import com.atlassian.confluence.pages.PageManager;
-import com.atlassian.confluence.xhtml.api.MacroDefinition;
-import com.atlassian.confluence.xhtml.api.MacroDefinitionHandler;
+import com.atlassian.confluence.rest.api.model.ExpansionsParser;
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
+import com.atlassian.confluence.user.ConfluenceUser;
+import com.atlassian.confluence.user.UserAccessor;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 import com.atlassian.migration.app.AccessScope;
 import com.atlassian.migration.app.PaginatedMapping;
@@ -24,11 +30,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static com.atlassian.migration.app.AccessScope.APP_DATA_OTHER;
 import static com.atlassian.migration.app.AccessScope.MIGRATION_TRACING_PRODUCT;
@@ -38,23 +43,31 @@ import static com.atlassian.migration.app.AccessScope.MIGRATION_TRACING_PRODUCT;
 public class MyPluginComponentImpl implements DiscoverableListener {
 
     private static final Logger log = LoggerFactory.getLogger(MyPluginComponentImpl.class);
-    @ConfluenceImport private final PageManager pageManager;
+    @ConfluenceImport private final ContentService contentService;
+    @ConfluenceImport private final UserAccessor userAccessor;
     @ConfluenceImport private final XhtmlContent xhtmlContent;
 
     @Inject
     public MyPluginComponentImpl(
-            @ConfluenceImport PageManager pageManager,
+            @ConfluenceImport ContentService contentService,
+            @ConfluenceImport UserAccessor userAccessor,
             @ConfluenceImport XhtmlContent xhtmlContent)
     {
         // It is not safe to save a direct reference to the gateway as that can change over time
-        this.pageManager = pageManager;
+        this.contentService = contentService;
+        this.userAccessor = userAccessor;
         this.xhtmlContent = xhtmlContent;
     }
 
     @Override
     public void onStartAppMigration(AppCloudMigrationGateway gateway, String transferId, MigrationDetailsV1 migrationDetails) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
+        try(final OutputStream stream = gateway.createAppData(transferId)) {
+            final boolean isSetupAdminUserSuccess = setupAdminUser();
+            if (!isSetupAdminUserSuccess) {
+                throw new RuntimeException("Please make sure confluence-administrators has at least 1 user");
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
             ObjectMapper overallMapper = new ObjectMapper();
             ObjectNode topLevelNode = overallMapper.createObjectNode();
 
@@ -69,34 +82,50 @@ public class MyPluginComponentImpl implements DiscoverableListener {
                     String serverPageId = entry.getKey();
                     String cloudPageId = entry.getValue();
 
-                    Page page = pageManager.getPage(Long.parseLong(serverPageId));
-                    if (page != null && page.isLatestVersion()) {
-                        final Boolean[] hasRelevantMacros = {false};
+                    final Optional<String> pageBodyOpt = getLatestPageBody(Long.parseLong(serverPageId));
 
-                        String body = page.getBodyAsString();
-                        xhtmlContent.handleMacroDefinitions(body, new DefaultConversionContext(new RenderContext()), new MacroDefinitionHandler() {
-                            @Override
-                            public void handle(MacroDefinition macroDefinition) {
-                                if ("markdown-from-url".equals(macroDefinition.getName())) {
-                                    hasRelevantMacros[0] = true;
-                                }
+                    if (pageBodyOpt.isPresent()) {
+                        xhtmlContent.handleMacroDefinitions(pageBodyOpt.get(), new DefaultConversionContext(new RenderContext()), macroDefinition -> {
+                            if ("markdown-from-url".equals(macroDefinition.getName())) {
+                                cloudPageIdsNode.add(cloudPageId);
                             }
                         });
-
-                        if (hasRelevantMacros[0]) {
-                            cloudPageIdsNode.add(cloudPageId);
-                        }
                     }
                 }
             }
 
-            OutputStream stream = gateway.createAppData(transferId);
             stream.write(overallMapper.writeValueAsString(topLevelNode).getBytes(StandardCharsets.UTF_8));
-            stream.close();
-
         } catch (Exception e) {
             log.error("Error while running app migration", e);
         }
+    }
+
+    private boolean setupAdminUser() {
+        final Optional<ConfluenceUser> adminUser = Optional
+                .ofNullable(userAccessor.getGroup("confluence-administrators"))
+                .flatMap(adminGroup -> StreamSupport.stream(
+                        Spliterators.spliteratorUnknownSize(
+                                userAccessor.getMembers(adminGroup).iterator(),
+                                Spliterator.ORDERED
+                        ),
+                        false
+                ).findFirst());
+        adminUser.ifPresent(AuthenticatedUserThreadLocal::set);
+
+        return adminUser.isPresent();
+    }
+
+    private Optional<String> getLatestPageBody(Long pageId) {
+        final Optional<Content> pageOpt = contentService
+                .find(ExpansionsParser.parse("history,body.storage"))
+                .withType(ContentType.PAGE)
+                .withId(ContentId.of(pageId))
+                .fetch();
+        return pageOpt
+                .filter(page -> page.getHistory().isLatest())
+                .map(Content::getBody)
+                .map(page -> page.get(ContentRepresentation.STORAGE))
+                .map(ContentBody::getValue);
     }
 
     @Override
