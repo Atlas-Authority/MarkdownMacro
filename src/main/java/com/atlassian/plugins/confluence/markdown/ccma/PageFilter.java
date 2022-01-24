52d484da24ab710d589cd3dfd14a3d27a3fcbc76
Nhan Nguyen <nhan.nguyen@atlasauthority.com>
2022-01-24 17:16:25 +0700

    Log error when failed to parse page body

--- src/main/java/com/atlassian/plugins/confluence/markdown/ccma/PageFilter.java
+++ src/main/java/com/atlassian/plugins/confluence/markdown/ccma/PageFilter.java
@@ -5,12 +5,16 @@ import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ConfluenceImport;
 import com.atlassian.renderer.RenderContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 class PageFilter {
+    private static final Logger log = LoggerFactory.getLogger(PageFilter.class);
+
     private final XhtmlContent xhtmlContent;
 
     @Inject
@@ -32,6 +36,7 @@ class PageFilter {
                         macroDefinition -> result = "markdown-from-url".equals(macroDefinition.getName()
                         ));
             } catch (XhtmlException e) {
+                log.error("Unable to parse content body", e);
                 result = false;
             }
             return result;
