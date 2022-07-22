d5f68ee6de16f56f1d51f092c163a4a48128b9f9
Kha Nguyen <kha.nguyen@atlasauthority.com>
2022-07-22 11:25:46 +0700

    SUPPORT-1675 switch to constructor injection

--- src/main/java/com/atlassian/plugins/confluence/markdown/configuration/PluginAdminSaveConfigurationAction.java
+++ src/main/java/com/atlassian/plugins/confluence/markdown/configuration/PluginAdminSaveConfigurationAction.java
@@ -3,17 +3,25 @@ package com.atlassian.plugins.confluence.markdown.configuration;
 import com.atlassian.bandana.BandanaManager;
 import com.atlassian.confluence.core.ConfluenceActionSupport;
 import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ConfluenceImport;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.inject.Inject;
 
 public class PluginAdminSaveConfigurationAction extends ConfluenceActionSupport {
     public static final String PLUGIN_CONFIG_KEY = "markdown-plugin-config-00";
 
-    private BandanaManager bandanaManager;
-    private ConfluenceBandanaContext context = new ConfluenceBandanaContext("markdown-plugin");
-    private ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private final BandanaManager bandanaManager;
+    private final ConfluenceBandanaContext context = new ConfluenceBandanaContext("markdown-plugin");
+    private final ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     private MacroConfigModel model;
+
+    @Inject
+    public PluginAdminSaveConfigurationAction(@ConfluenceImport BandanaManager bandanaManager) {
+        this.bandanaManager = bandanaManager;
+    }
 
     @Override
     public String execute() throws Exception {
@@ -25,18 +33,14 @@ public class PluginAdminSaveConfigurationAction extends ConfluenceActionSupport 
         if (oldModel == null) {
         	oldModel = new MacroConfigModel();
         }
-    	
+
         if (model != null) {
         	if (!oldModel.getConfig().equals(model.getConfig())) model.setIsChanged(true);
         	else model.setIsChanged(false);
         }
-    	
+
         bandanaManager.setValue(context, PLUGIN_CONFIG_KEY, objectMapper.writeValueAsString(model));
         return SUCCESS;
-    }
-
-    public void setBandanaManager(BandanaManager bandanaManager) {
-        this.bandanaManager = bandanaManager;
     }
 
     public void setData(String data) throws Exception {
